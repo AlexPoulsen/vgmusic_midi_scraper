52560b5d0cbae2ab3d99d78c20d9d5cbbbf0cd28
squivix <55770995+squivix@users.noreply.github.com>
2020-10-02 21:12:26 +0300

    Update dl_vgmusic.py
    Made it save the midi files to sub directories for the company and console.

--- dl_vgmusic.py
+++ dl_vgmusic.py
@@ -21,17 +21,18 @@ with open("vgmusic_sources.txt", "r") as pagelist:
 					print("\n", end="     ")
 				counter += 1
 			print("\n")
-path = pathlib.Path(".") / "vgmusic_sources"
-try:
-	os.makedirs(path)
-except OSError as e:
-	if e.errno != errno.EEXIST:
-		raise
 counter = 1
 print("downloading files")
 print("     ", end="")
 for file in dl:
-	urllib.request.urlretrieve(file, path / file.split("/")[-1])
+    tokens=file.split("/")
+    path = pathlib.Path(".") / "vgmusic_sources"/ tokens[-3]/tokens[-2]
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+	urllib.request.urlretrieve(file, path / tokens[-1])
 	print("|", end="")
 	if counter % 120 == 0:
 		print("\n", end="     ")
